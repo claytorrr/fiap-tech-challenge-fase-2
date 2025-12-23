ce357e6be64a8c72874536b2bf95a2fde53c450c
claytorrr <claytonabgomes@gmail.com>
2025-12-22 22:18:14 -0300

    feat: adiciona integração inicial com MongoDB usando Mongoose

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Conexão com o MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err);
+    process.exit(1);
+  });
 
 app.get('/', (req, res) => {
   res.send('API Tech Challenge rodando!');
